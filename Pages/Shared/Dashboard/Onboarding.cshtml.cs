ceeec18b82db9a9ca1a89de48dc874d8e518c7fa
lash0000 <k80308392@gmail.com>
2024-10-24 21:45:57 +0800

    Sign In and Dashboard only

---
+++ Pages/Shared/Dashboard/Onboarding.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SAMS_IPT102.Pages.Dashboard
+{
+    public class OnboardingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
